05602b467544bcbf7b76baa6c3744f4dff68be5e
armstrongmsg <armstrongmsg@gmail.com>
2017-06-12 16:54:51 -0300

    Resolved the log duplication problem

--- application_manager/utils/logger.py
+++ application_manager/utils/logger.py
@@ -1,19 +1,18 @@
 import logging
-import time
-
 
 class Log:
     def __init__(self, name, output_file_path):
         self.logger = logging.getLogger(name)
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.DEBUG)
-        self.logger.addHandler(handler)
-        handler = logging.FileHandler(output_file_path)
-        self.logger.addHandler(handler)
+        if not len(self.logger.handlers):
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.INFO)
+            self.logger.addHandler(handler)
+            handler = logging.FileHandler(output_file_path)
+            self.logger.addHandler(handler)
 
     def log(self, text):
         self.logger.info(text)
 
 
 def configure_logging():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
