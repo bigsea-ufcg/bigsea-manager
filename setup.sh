ce11f4d2d051d51cb736d9110feff187ac9d33f0
Ubuntu <ubuntu@refactoring.lsd.ufcg.edu.br>
2018-04-24 16:47:32 -0300

    Update requirements.txt and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 sudo apt-get update
-
-sudo apt-get -y install libffi-dev
-sudo apt-get -y install libssl-dev
-sudo apt-get -y install python-tox
 sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
+sudo pip install tox
